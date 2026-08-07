de74168235164e38043ff8c8b29d99aeb616b59e
Aman Kumar <amankr.dev17@gmail.com>
2026-08-07 19:54:41 +0530

    pattern 5

--- Pattern Problems/pattern5.java
+++ Pattern Problems/pattern5.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
-public class pattern1 {
+public class pattern5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
 
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=i; j++){
+        for(int i=1; i<=n; i++){
+            for(int j=n; j<1;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-}
+    }
 }
