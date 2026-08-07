cc0c3d6562f01a8155e27e5072f15e6ec3a2e5db
Aman Kumar <amankr.dev17@gmail.com>
2026-08-08 00:25:37 +0530

    pattern 13

--- pattern13.java
+++ pattern13.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
-public class pattern3 {
+public class pattern13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
-
+        int a = 0;
         for(int i=0; i<n; i++){
             for(int j=1; j<=i+1; j++){
-                System.out.print(j);
+                a++;
+                System.out.print(a+ " ");
             }
             System.out.println();
         }
