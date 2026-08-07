2f5c60c5505894d01221ddffe36cc9ef005c513b
Aman Kumar <amankr.dev17@gmail.com>
2026-08-07 18:38:31 +0530

    pattern 1

--- Pattern Problems/pattern1.java
+++ Pattern Problems/pattern1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class pattern {
+public class pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
