45bca7d43d63b1242e13214ca13044f97ec10bcb
Aman Kumar <amankr.dev17@gmail.com>
2026-08-07 22:30:40 +0530

    pattern 8

--- Pattern Problems/pattern8.java
+++ Pattern Problems/pattern8.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
-public class pattern7 {
+public class pattern8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
 
-        for(int i=0; i<n; i++){
-            for(int k=1; k<n-i; k++){
+        for(int i=n; i>=1; i--){
+            for(int j=n; j>i; j--){
                 System.out.print(" ");
             }
-            for(int j=1; j<=(2*i+1); j++){
+            for(int k=2*i-1; k>=1; k--){
                 System.out.print("*");
             }
             System.out.println();
