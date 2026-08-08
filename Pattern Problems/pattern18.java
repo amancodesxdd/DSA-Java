acffceeb68b7ae729166abed2eb9de4b0622bedf
Aman Kumar <amankr.dev17@gmail.com>
2026-08-08 23:07:39 +0530

    pattern 19

---
+++ Pattern Problems/pattern18.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pattern18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            char c = (char) ('A'+ n-i);
+            for(int j =1;j<=i;j++){
+                System.out.print(c+ " ");
+                c++;
+            }
+            System.out.println();
+        }
+
+
+    }
+}
--- Pattern Problems/pattern19.java
+++ Pattern Problems/pattern19.java
@@ -1,19 +1,39 @@
-import java.util.*;
 public class pattern19 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        int n = 5;
 
-        for(int i=1;i<=n;i++){
-            char c = (char) ('A'+ n-i);
-            for(int j =1;j<=i;j++){
-                System.out.print(c+ " ");
-                c++;
+        for(int i=0; i<n ; i++){
+            
+           for(int j=i; j<n; j++){
+                System.out.print("*");
             }
+
+            for(int j=0;j<2*i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j<n; j++){
+                System.out.print("*");
+            }
+            
+            
             System.out.println();
         }
 
+        for(int i=1; i<=n ; i++){
+            
+           for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
 
+            for(int j=0;j<2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            
+            
+            System.out.println();
+        }
     }
 }
