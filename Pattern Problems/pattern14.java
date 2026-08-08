74e94ca04c0a5cf2dfba86fb96f1b08bc3206ca4
Aman Kumar <amankr.dev17@gmail.com>
2026-08-08 18:38:53 +0530

    pattern 14

--- Pattern Problems/pattern14.java
+++ Pattern Problems/pattern14.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class pattern6 {
+public class pattern14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
 
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=i; j++){
-                System.out.print(j);
+        for(int i=0; i<n; i++){
+            for(char c='A'; c<='A'+i; c++){
+                System.out.print(c);
             }
             System.out.println();
         }
