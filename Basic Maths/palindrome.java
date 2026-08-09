bb827b4d60993c86019ac443fce52f28b45b5be0
Aman Kumar <amankr.dev17@gmail.com>
2026-08-09 12:38:07 +0530

    check palindrome or not

--- Basic Maths/palindrome.java
+++ Basic Maths/palindrome.java
@@ -1,5 +1,27 @@
-package Basic Maths;
+import java.util.Scanner;
 
 public class palindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int n = num;
+        
+        int newnum = 0;
+
+    while(num!=0){
+        int temp = num%10;
+        newnum = newnum*10+temp;
+        num = num/10;
+    }
+
+    if(n == newnum){
+        System.out.println("True");
+    }else{
+        System.out.println("False");
+    }
+
+        
+
+    }
 }
