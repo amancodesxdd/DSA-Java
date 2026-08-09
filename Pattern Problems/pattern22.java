90199ffe60869808e3a2ca4f999709158fc1b9ae
Aman Kumar <amankr.dev17@gmail.com>
2026-08-09 10:49:23 +0530

    pattern 22

---
+++ Pattern Problems/pattern22.java
@@ -0,0 +1,19 @@
+public class pattern22 {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int top = i;
+                int left = j;
+                int right = (2*n-2) - j;
+                int bottom = (2*n-2) - i;
+
+                int mindist = Math.min(Math.min(top,bottom),Math.min(left,right));
+                
+                System.out.print(n-mindist);
+            }   
+            System.out.println();  
+    }
+}
+}
