9dcbf8266873abdddd136596b6da8963920d6b59
Aman Kumar <amankr.dev17@gmail.com>
2026-08-12 18:49:10 +0530

    recursion q1

---
+++ Recursion/rec1.java
@@ -0,0 +1,15 @@
+package Recursion;
+import java.util.*;
+
+public class rec1 {
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+    static void ntimes(){
+        for(int i = 0; i < n; i++){
+            System.out.println("Rose");
+        }
+    }
+    public static void main(String[] args) {
+        ntimes();
+    }
+}
