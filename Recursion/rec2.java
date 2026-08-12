ca152c2b18b3303405a01c62c5115a18c7dc5a95
Aman Kumar <amankr.dev17@gmail.com>
2026-08-12 18:54:10 +0530

    recursion q2

---
+++ Recursion/rec2.java
@@ -0,0 +1,15 @@
+package Recursion;
+import java.util.*;
+
+public class rec2 {
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+    static void ntimes(){
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        ntimes();
+    }
+}
