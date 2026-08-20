05e8889452690c0f52a553372469aa497fcc5888
Aman Kumar <amankr.dev17@gmail.com>
2026-08-20 08:02:48 +0530

    insertion sorting

--- Sorting/selectionsort.java
+++ Sorting/selectionsort.java
@@ -2,35 +2,31 @@ package Sorting;
 
 import java.util.Scanner;
 
-public class insertionsort {
+public class selectionsort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
         int[] arr = new int[5];
 
         for(int i=0; i<arr.length;i++){
             arr[i]= sc.nextInt();
         }
 
-        for(int i = 1; i < arr.length; i++){
-            int j = i;
-
-            while(j>0 && arr[j] < arr[j-1]){
-                int temp = arr[j-1];
-                arr[j-1] = arr[j];
-                arr[j] = temp;
-
-                j--;
-            }
+        for(int i=0; i<=arr.length-2;i++){
+            int min = i;
             
+            for(int j=i; j<=arr.length-1;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
-         System.out.print("Sorted Array:");
+        System.out.print("Sorted Array:");
         for (int i : arr) {
             System.out.print(i+ " ");
         }
         sc.close();
     }
-    
-
 }
-
