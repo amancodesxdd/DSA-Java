a6823d520bcaa7dc0d10d16486a3d3fa59476bd9
Aman Kumar <amankr.dev17@gmail.com>
2026-08-22 23:02:48 +0530

    quick sort

---
+++ Sorting/quicksort2.java
@@ -0,0 +1,45 @@
+package Sorting;
+
+public class quicksort2 {
+    public static void qs(int[] arr, int low, int high) {
+        if(low<high){
+            int pindex = f(arr, low, high);
+            qs(arr, low, pindex-1);
+            qs(arr,pindex+1,high);
+        }
+    }
+    public static int f(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        arr[low] = arr[j];
+        arr[j] = pivot;
+        return j;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,2,6,7,1,4,8,3};
+
+        qs(arr,0,arr.length - 1);
+
+        for (int i : arr) {
+            System.out.print(i+ " ");
+        }
+    }
+
+
+}
