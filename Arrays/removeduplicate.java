db8c5ee69774e549d90423a255b79c092afeeaca
Aman Kumar <amankr.dev17@gmail.com>
2026-08-25 20:16:40 +0530

    remove duplicate from array

---
+++ Arrays/removeduplicate.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class removeduplicate {
+    public static int duplicate(int[] arr){
+        int count = 1;
+        for(int i = 0; i < arr.length-1; i ++ ){
+            if(arr[i] != arr[i+1]){
+              count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,0,3,3,5,6};
+
+        System.out.println(duplicate(arr));
+    }
+}
