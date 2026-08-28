be48f22b4edd92b0b9bab4b0f6e28fed0bedd5fc
Aman Kumar <amankr.dev17@gmail.com>
2026-08-29 00:16:24 +0530

    left rotate by k

--- Arrays/leftrotatebyk.java
+++ Arrays/leftrotatebyk.java
@@ -2,21 +2,24 @@ package Arrays;
 
 public class leftrotatebyk {
 
-    public static void rotate(int[] arr) {
-        int k = 2;
-        for (int j = 1; j<=k; j++){
+    public static void rotate(int[] arr,int k) {
+        k = k % arr.length;
+        int[] temp = new int[k];
 
-            for(int i = 0; i <=arr.length-2; i ++){
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-}
-    }
+        for(int i = 0; i < k; i++){
+            temp[i] = arr[i];
+        }
+        for(int j=k; j <arr.length; j++){
+            arr[j-k] = arr[j];
+        }
+        for(int a = arr.length-k;a <arr.length; a++){
+            arr[a] = temp[a-(arr.length-k)];
+        }
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
 
-        rotate(arr);
+        rotate(arr,3);
 
         for (int i : arr) {
             System.out.print(i + " ");
