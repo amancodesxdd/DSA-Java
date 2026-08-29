34716ac6491f6c824cfa7579c96b6a2d8a4295f8
Aman Kumar <amankr.dev17@gmail.com>
2026-08-30 00:37:03 +0530

    move all zeroes to end

--- Arrays/moveallzerotoend.java
+++ Arrays/moveallzerotoend.java
@@ -2,17 +2,17 @@ package Arrays;
 
 public class moveallzerotoend {
 
-    public static void rotate(int[] arr) {
-        for(int i = 0;i < arr.length;i++ ){
-          if(arr[i] ==0 ){
-            for(int k = i;k < arr.length-1;k++ ){
-                int temp = arr[k];
-                arr[k] = arr[k + 1];
-                arr[k + 1] = temp; 
+    public static void rotate(int[] nums) {
+       for(int i = 0;i < nums.length-1;i++ ){
+            for(int k = 0;k < nums.length-1;k++ ){
+                if(nums[k] ==0 ){
+                int temp = nums[k];
+                nums[k] = nums[k + 1];
+                nums[k + 1] = temp;} 
         }
     }
-}
-}
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,0,3,0,5,6};
 
