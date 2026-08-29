968373591cd6b490851ec81f03f420317d752d92
Aman Kumar <amankr.dev17@gmail.com>
2026-08-30 00:51:04 +0530

    linear search

---
+++ Arrays/linearsearch.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class linearsearch {
+    public static int ls(int[] arr, int k){
+        for(int i = 0; i < arr.length; i ++){
+            if(arr[i] == k){
+                return i;
+                
+            }
+        }
+        return -1;
+    }
+}
