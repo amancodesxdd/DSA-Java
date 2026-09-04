46e0f682df013cc39568025d5252b394b276cf7f
Aman Kumar <amankr.dev17@gmail.com>
2026-09-04 19:04:21 +0530

    Find the number that appears once, and the other numbers twice

---
+++ Arrays/singlenumber1.java
@@ -0,0 +1,43 @@
+// public class singlenumber1 {
+//     public static int single(int[] nums) {
+//         int count = 0;
+//         for(int i = 0; i < nums.length; i++){
+//             for (int j = 0; j < nums.length; j++) {
+//                 if(nums[i] == nums[j]){
+//                     count++;
+//                 }
+//             }
+//         if (count < 2) {
+//             return nums[i];
+//         }
+//         count= 0;
+        
+//         }
+//         return 0;
+
+//     }
+//     public static void main(String[] args) {
+//         int[] nums = {1,2,2,4,3,1,4};
+
+//         System.out.println(single(nums));
+//     }
+// }
+
+public class singlenumber1 {
+    public static int single(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            count ^= nums[i];
+            }
+            return count;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,4,3,1,4};
+
+        System.out.println(single(nums));
+    }
+}
+
+
+
