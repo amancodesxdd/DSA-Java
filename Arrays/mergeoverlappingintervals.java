4d0e78a105a74e53ffeada590539e62f4597fd78
Aman Kumar <amankr.dev17@gmail.com>
2026-09-14 20:29:21 +0530

    merge overlapping intrevals

---
+++ Arrays/mergeoverlappingintervals.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class mergeoverlappingintervals{
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+            return a[0] - b[0];
+        }
+        });
+
+        List <int[]> list = new ArrayList<>();
+
+        for(int[] interval : intervals){
+            if(list.isEmpty() || list.get(list.size() - 1)[1] < interval[0]){
+                list.add(interval);
+            }
+            else{
+                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], interval[1]);
+            }
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+    public static void main(String[] args) {
+        int[][] list = {{1,5},{3,6},{8,10},{15,18}};
+        int [][] result = merge(list);
+        
+        for (int[] is : result) {
+            System.out.print(Arrays.toString(is));
+        }
+    }
+}
