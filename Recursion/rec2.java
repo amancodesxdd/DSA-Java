fd77a263e5c393a7077e582e5921033ad2aeec33
Aman Kumar <amankr.dev17@gmail.com>
2026-09-18 19:00:23 +0530

    print 1 to n using recursion

--- Recursion/rec2.java
+++ Recursion/rec2.java
@@ -1,15 +1,13 @@
-package Recursion;
-import java.util.*;
-
 public class rec2 {
-    static Scanner sc = new Scanner(System.in);
-    static int n = sc.nextInt();
-    static void ntimes(){
-        for(int i = 1; i <= n; i++){
-            System.out.println(i);
+    public static void ntimes(int n){
+        if(n == 0){
+            return;
         }
-    }
+
+        ntimes(n-1);
+    System.out.println(n);   
+ }
     public static void main(String[] args) {
-        ntimes();
+        ntimes(10);
     }
 }
