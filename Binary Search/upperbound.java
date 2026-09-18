450d69963f3ebdadd2b6232af6ae91461fe2841c
Aman Kumar <amankr.dev17@gmail.com>
2026-09-19 00:01:20 +0530

    upper bound using binary search

--- Binary Search/upperbound.java
+++ Binary Search/upperbound.java
@@ -1,11 +1,10 @@
-public class lowerbound{
+public class upperbound{
     public static int lowerb(int[] nums, int low, int high, int target) {
         int mid = (low + high)/ 2;
-        
         if(low > high){
             return low;
         }
-        if (nums[mid] >= target) {
+        if (nums[mid] > target) {
             return lowerb(nums, low, mid - 1, target);
         }
         else{
@@ -14,6 +13,6 @@ public class lowerbound{
     }
     public static void main(String[] args) {
         int[] nums = {3,5,8,15,19};
-        System.out.println(lowerb(nums, 0, nums.length, 4));
+        System.out.println(lowerb(nums, 0, nums.length, 9));
     }
 }
