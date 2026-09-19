8014dac2aab85bf6fa13f952879a03cd6e110569
Aman Kumar <amankr.dev17@gmail.com>
2026-09-19 23:32:35 +0530

    count occurences using binary search

---
+++ Binary Search/countocc.java
@@ -0,0 +1,40 @@
+public class countacc{
+        public static int countOccurrences(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int first = -1;
+        int last = -1;
+        
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = nums.length - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                last = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return (last - first + 1);
+}
+}
